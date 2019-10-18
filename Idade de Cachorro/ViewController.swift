840e9bc296cca68883f2fb7ed2118745a647ba9f
Matheus Rodrigues Araujo <matheusaraujo@NBBV016619.BRQ.COM>
2019-10-18 16:00:19 -0300

    Adicionando App completo

---
+++ Idade de Cachorro/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Idade de Cachorro
+//
+//  Created by Matheus Rodrigues Araujo on 11/10/19.
+//  Copyright © 2019 Curso IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro é: " + String(idade)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
